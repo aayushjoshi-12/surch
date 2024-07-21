23c0e84c881ecf76fbaf389aacd9ad5bcbfd957c
Aayush Joshi <joshiaayush1244@gmail.com>
2024-07-21 20:54:55 +0530

    used endpoints for faster inference and created a ui

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# surch
-my attempt at creating an RAG application
+# Surch:mag:...
+
+Surch is a Streamlit-based web application designed to simplify the process of finding answers to your questions using Google's Custom Search Engine and leveraging the power of Hugging Face's language models for concise, accurate responses.
---
+++ inference.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+import requests
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+import streamlit as st
+
+api_key = st.secrets['API_KEY']
+cse_id = st.secrets["CSE_ID"]
+access_token = st.secrets["ACCESS_TOKEN"]
+
+template = """
+system: you are a summarizer that finds the answer the user is looking for in the context provided by the google search results top links.
+keep your answers factual and only generate one response.
+give the answer in markdown format.
+context: {context}
+user: {prompt}
+"""
+
+prompt = PromptTemplate.from_template(template)
+
+repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
+
+llm = HuggingFaceEndpoint(
+    repo_id=repo_id,
+    temperature=0.5,
+    huggingfacehub_api_token=access_token,
+)
+
+llm_chain = prompt | llm
+
+
+def google_search(query, api_key, cse_id, num_results=3):
+    url = "https://www.googleapis.com/customsearch/v1"
+    params = {"q": query, "key": api_key, "cx": cse_id, "num": num_results}
+    response = requests.get(url, params=params)
+    results = response.json().get("items", [])
+    return results
+
+
+def get_context(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    paragraphs = ""
+    paragraphs = "".join([p.get_text() for p in soup.find_all("p")[:10]])
+    return paragraphs
+
+
+def get_answer(prompt):
+    top_url = [
+        result["link"]
+        for result in google_search(prompt, api_key, cse_id, num_results=3)
+    ]
+    context = r"".join([get_context(url) for url in top_url])
+    answer = llm_chain.invoke({"context": context, "prompt": prompt})
+    return answer
